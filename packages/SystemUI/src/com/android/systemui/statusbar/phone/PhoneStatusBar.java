0d557f6ad1d6dbef908bc9edb95ed4912332c439
ghbhaha <ghbhaha@gmail.com>
2015-11-17 15:01:44 +0800

    base:StatusBarNetworkTraffic(1/2)
    Change-Id: I791b6210660adb9cf7fb04a9e03bf7582582702a
    
    Conflicts:
    	packages/DocumentsUI/res/values/cm_dimens.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010 The Android Open Source Project
+ * Copyright (C) 2015 The SudaMod Project 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -172,6 +173,7 @@ import com.android.systemui.statusbar.policy.LiveLockScreenController;
 import com.android.systemui.statusbar.policy.LocationControllerImpl;
 import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.NetworkControllerImpl;
+import com.android.systemui.statusbar.policy.NetworkTraffic;
 import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.PreviewInflater;
 import com.android.systemui.statusbar.policy.RotationLockControllerImpl;
@@ -371,6 +373,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private Bitmap mKeyguardWallpaper;
 
     int mKeyguardMaxNotificationCount;
+
+    // Status bar Network traffic;
+    private NetworkTraffic mNetworkTraffic;
 
     boolean mExpandedVisible;
 
@@ -986,6 +991,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         // figure out which pixel-format to use for the status bar.
         mPixelFormat = PixelFormat.OPAQUE;
+
+
+        mNetworkTraffic = (NetworkTraffic)mStatusBarView.findViewById(R.id.network_traffic);
 
         mStackScroller = (NotificationStackScrollLayout) mStatusBarWindowContent.findViewById(
                 R.id.notification_stack_scroller);
@@ -3712,6 +3720,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         animateCollapsePanels();
         updatePublicMode();
+        mNetworkTraffic.updateSettings();
         updateNotifications();
         resetUserSetupObserver();
         setControllerUsers();
