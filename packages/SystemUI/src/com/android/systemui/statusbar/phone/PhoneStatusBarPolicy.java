892d9d2e690f039027c9f45865f6ab4f405f3886
Eugene Chan <hylpcyj@gmail.com>
2017-05-05 17:50:40 +0800

    Fix build

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
@@ -87,6 +87,7 @@ public class PhoneStatusBarPolicy implements Callback {
     private static final String SLOT_ALARM_CLOCK = "alarm_clock";
     private static final String SLOT_MANAGED_PROFILE = "managed_profile";
     private static final String SLOT_SU = "su";
+    private static final String SLOT_HEADSET = "headset";
 
     private final Context mContext;
     private final StatusBarManager mService;
